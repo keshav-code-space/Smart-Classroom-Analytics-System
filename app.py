ba4a04f41408b6b395c71ac38799366a3c7e040d
Keshav Soni <keshav.soni200x@gmail.com>
2026-06-13 16:34:20 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+#problem statement:
+# mger problem is how to create this model or interface to work on need to learn some topic before trying it like pandas and advance skit-learn.
+
+
+#this is the main terminl for the app to work on and to connect with the model.
