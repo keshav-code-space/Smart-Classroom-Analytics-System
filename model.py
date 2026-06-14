c106a259401d5bb8279b79522b2169845df77c2d
Keshav Soni <keshav.soni200x@gmail.com>
2026-06-14 17:50:12 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,4 @@
+# here is all the content for the Ai model is build this model main purpose is to predect the pass and failing of the student before the term end exam.
+# the modle usese the deep knowledge skit-learn , pandas and basic dsa , python implementation.
+# hopfully this is all i need for the time being.
+😉.
